650e31352018ec06648d02077f7005b043531d3a
Code Hugger (Matthew Jones) <jonespm@umich.edu>
2019-01-24 14:01:45 -0500

    Some updates to move this into env files, only tested with lti-tools

--- caliper_tester.py
+++ caliper_tester.py
@@ -10,46 +10,49 @@
 
 from __future__ import unicode_literals
 import caliper
-import requests, json, sys
+import requests, json, sys, os, logging
 
 from datetime import datetime
+from dotenv import load_dotenv
+
+logging.basicConfig(stream=sys.stdout, level=logging.INFO)
+logger = logging.getLogger()
+
+# Add this path first so it picks up the newest changes without having to rebuild
+this_dir = os.path.dirname(os.path.abspath(__file__))
+sys.path.insert(0, this_dir + "/..")
+load_dotenv(dotenv_path=this_dir + "/.env")
 
 # Configuration is for OpenLRW, obtain bearer token
 
-# For OPENLRW
-#lrw_server = "http://localhost:9966"
-#lrw_access = f"{lrw_server}/api/auth/login"
-#lrw_endpoint = f"{lrw_server}/api/caliper"
+lrw_type = os.getenv('LRW_TYPE',"").lower()
+token = os.getenv('TOKEN',"")
+lrw_server = os.getenv('LRW_SERVER', "")
 
-# For Unizin
-#lrw_server = "https://umich.caliper.dev.cloud.unizin.org"
-#lrw_access = ""
-#lrw_endpoint = f"{lrw_server}"
-#token = ""
-
-is_openlrw = False
-
-lrw_server = "http://lti.tools"
-# Not needed for test server
 lrw_access = ""
-lrw_endpoint = f"{lrw_server}/caliper/event?key=python-caliper"
-token = "python-caliper"
 
-# This is needed to get a token from the access point
-if (is_openlrw):
+if lrw_type == 'unizin':
+    lrw_endpoint = f"{lrw_server}"
+elif lrw_type == 'ltitools':
+    lrw_endpoint = f"{lrw_server}/caliper/event?key={token}"
+elif lrw_type == 'openlrw':
+    lrw_access = f"{lrw_server}/api/auth/login"
+    lrw_endpoint = f"{lrw_server}/api/caliper"
     auth_data = {'username':'a601fd34-9f86-49ad-81dd-2b83dbee522b', 'password':'e4dff262-1583-4974-8d21-bff043db34d5'}
     r = requests.post(f"{lrw_access}", json = auth_data, headers={'X-Requested-With': 'XMLHttpRequest'})
     token = r.json().get('token')
+else:
+    sys.exit(f"LRW Type {lrw_type} not supported")
 
 the_config = caliper.HttpOptions(
-      host=f"{lrw_endpoint}",
-      auth_scheme='Bearer',
-      api_key=token, 
-      debug=False)
+    host=f"{lrw_endpoint}",
+    auth_scheme='Bearer',
+    api_key=token, 
+    debug=True)
 
 # Here you build your sensor; it will have one client in its registry,
 # with the key 'default'.
-the_sensor = caliper.build_sensor_from_config(
+the_sensor = caliper.build_simple_sensor(
         sensor_id = f"runestone",
         config_options = the_config )
 
@@ -74,20 +77,8 @@ the_event = caliper.events.ViewEvent(
 # entities; suppose for example, you'll be sending a number of events
 # that all have the same actor
 
-print (dir(the_event))
-# The return structure from the sensor will be a dictionary of lists: each
-# item in the dictionary has a key corresponding to a client key,
-# so ret['default'] fetches back the list of URIs of all the @ids of
-# the fully described Caliper objects you have sent with that describe call.
-#
-# Now you can use this list with event sendings to send only the identifiers
-# of already-described entities, and not their full forms:
-#print(the_sensor.send(the_event, described_objects=ret['default'])
+logger.info(dir(the_event))
+logger.info(the_sensor.send(the_event))
 
-# You can also just send the event in iten full form, with all fleshed out
-# entities:
-print(the_sensor.send(the_event))
-
-for k, client in the_sensor.client_registry.items():
-        for debug in client.debug:
-                print ("Client {0} last status {1} {2} ".format(client.apiKey, debug.status_code, debug.text))
+logger.info (the_sensor.status_code)
+logger.info (the_sensor.debug)
